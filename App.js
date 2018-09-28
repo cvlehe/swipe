0246d0c7cae1a19851a887723ebf83317e773116
Charlie von Lehe <vonlehec@gmail.com>
2018-09-28 16:31:06 -0400

    Added circle animation

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Ball from "./src/Ball";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Ball style={styles.container}/>
     );
   }
 }
@@ -15,7 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
